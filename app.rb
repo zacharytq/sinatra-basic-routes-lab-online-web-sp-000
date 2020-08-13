1dd9b295b3482c164321c10f3d43ebf61bcb0bbc
chic-page-6502 <zacharytq@gmail.com>
2020-08-13 14:18:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Zach."
+  end
+
+  get '/hometown' do
+    "My hometown is WPB."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is WAP."
+  end
 end
